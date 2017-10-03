62c1cc51dda78a33f4624b70a23046c2f3b9ca7e
Paul Nguyen <panguyen@paloaltonetworks.com>
2017-10-03 10:47:12 -0700

    Fixed error messages to be more technical

--- bin/input_module_aperture.py
+++ bin/input_module_aperture.py
@@ -31,7 +31,7 @@ def validate_input(helper, definition):
 
 
 def get_auth_token(helper, opt_global_account, proxy_enabled):
-    helper.log_debug("GET AUTH TOKEN")
+    helper.log_debug("Start get_auth_token.")
     client_id = opt_global_account['username']
     secret = opt_global_account['password']
     url = "https://api.aperture.paloaltonetworks.com/oauth/token"
@@ -50,14 +50,14 @@ def get_auth_token(helper, opt_global_account, proxy_enabled):
                                         timeout=30, use_proxy=proxy_enabled)
     r_status = response.status_code
     if r_status == 200:
-        helper.log_debug('We got a token.')
+        helper.log_debug('Token recieved')
         token = response.json()['access_token']
         return token
     elif r_status == 401:
-        helper.log_error('Check Credentials')
+        helper.log_error('ERROR: Invalid credentials.')
         raise ValueError(r_status)
     else:
-        helper.log_error('Unable to retrieve token.')
+        helper.log_error('ERROR: Unable to retrieve token.')
         helper.log_debug(r_status)
         raise ValueError(r_status)
 
@@ -68,9 +68,9 @@ def collect_events(helper, ew):
     opt_global_account = helper.get_arg('global_account')
     proxy_settings = helper.get_proxy()
     proxy_enabled = bool(proxy_settings)
-    helper.log_debug("IS PROXY ENABLED?")
+    helper.log_debug("Checking if Proxy is enabled")
     helper.log_debug(proxy_enabled)
-    helper.log_debug("INPUT TYPE IS:")
+    helper.log_debug("Current input type is set to:")
     helper.log_debug(helper.get_input_stanza_names())
     token = get_auth_token(helper, opt_global_account, proxy_enabled)
     headers = {'Authorization': 'Bearer ' + token}
@@ -78,18 +78,16 @@ def collect_events(helper, ew):
     url = "https://api.aperture.paloaltonetworks.com/api/v1/log_events"
     r_status = 200
     while r_status != 204:
-        helper.log_debug('Continue to call rest api until we get a 204.')
         response = helper.send_http_request(
             url, method, parameters=None, payload=None,
             headers=headers, cookies=None, verify=True, cert=None,
             timeout=30, use_proxy=proxy_enabled)
         r_status = response.status_code
-        helper.log_debug('STATUS IS')
+        helper.log_debug('API status code is:')
         helper.log_debug(r_status)
         if r_status == 200:
-            helper.log_debug("Add data to index")
+            helper.log_debug("Adding data to index.")
             data = response.json()
-            helper.log_debug('DATA IS:')
             helper.log_debug(data)
             timestamp = datetime.datetime.strptime(data['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
             final_time = (timestamp - datetime.datetime.fromtimestamp(0)).total_seconds()
@@ -104,10 +102,11 @@ def collect_events(helper, ew):
             ew.write_event(event)
             time.sleep(3)
         elif r_status == 204:
-            helper.log_debug("Nothing New.")
+            helper.log_debug("STATUS 204: No new events were found.")
             break
         elif r_status >= 400:
+            helper.log_debug("ERROR Status is:")
             helper.log_debug(r_status)
             raise ValueError(r_status)
         else:
-            helper.log_error('There was a problem with the Aperture API call')
+            helper.log_error('There was a problem when trying to collect events using the aperture API call.')
