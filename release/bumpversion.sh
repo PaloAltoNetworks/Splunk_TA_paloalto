4d7e7df15cda182403b981b3ff27960c4439515a
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2018-10-26 11:22:01 -0700

    Modify github release scripts

--- release/bumpversion.sh
+++ release/bumpversion.sh
@@ -6,11 +6,11 @@ set -e
 
 display_usage() {
     echo "Bump the version"
-    echo -e "\nUsage:\nbumpversion.sh <new-app-version> <required-add-on-version> \n"
+    echo -e "\nUsage:\nbumpversion.sh <new-addon-version> \n"
 }
 
 # if less than two arguments supplied, display usage
-if [  $# -le 1 ]
+if [  $# -le 0 ]
 then
     display_usage
     exit 1
@@ -24,7 +24,6 @@ then
 fi
 
 VERSION=$1
-ADDON_VERSION=$2
 CURRENT_VERSION=`grep -o '^version = [0-9a-z.-]*' default/app.conf | awk '{print $3}'`
 BUILD=${VERSION//./}0
 echo "Bumping version from ${CURRENT_VERSION} to ${VERSION} build ${BUILD}"
@@ -37,11 +36,7 @@ sed -i '' -E 's/version = .+/version = '${VERSION}'/' ${APPCONF} ${APPCONF}
 echo "Bump ${APPCONF} version to ${VERSION}"
 sed -i '' -E 's/build = .+/build = '${BUILD}'/' ${APPCONF} ${APPCONF}
 echo "Bump ${APPCONF} build to ${BUILD}"
-sed -i '' -E 's/ta_dependency_version = .+/ta_dependency_version = '${ADDON_VERSION}'/' ${APPCONF} ${APPCONF}
-echo "Bump ${APPCONF} add-on required version to ${ADDON_VERSION}"
-sed -i '' -E 's/App Version:\*\* .+/App Version:** '${VERSION}'/' ${README} ${README}
+sed -i '' -E 's/Add-on Version:\*\* .+/Add-on Version:** '${VERSION}'/' ${README} ${README}
 echo "Bump ${README} Version to ${VERSION}"
-sed -i '' -E 's/Splunk_TA_paloalto .+/Splunk_TA_paloalto '${ADDON_VERSION}'/' ${README} ${README}
-echo "Bump ${README} add-on required version to ${ADDON_VERSION}"
 
 
--- release/github.sh
+++ release/github.sh
@@ -10,15 +10,8 @@ set -e
 
 display_usage() {
     echo "Bump the version, commit, and merge with master"
-    echo -e "\nUsage:\ngithub.sh <new-app-version> <required-add-on-version> \n"
+    echo -e "\nUsage:\ngithub.sh <new-addon-version> \n"
 }
-
-# if less than two arguments supplied, display usage
-if [  $# -le 1 ]
-then
-    display_usage
-    exit 1
-fi
 
 # check whether user had supplied -h or --help . If yes display usage
 if [[ ( $# == "--help") ||  $# == "-h" ]]
@@ -27,10 +20,14 @@ then
     exit 0
 fi
 
+if [  $# -le 0 ]
+then
+    display_usage
+    exit 1
+fi
+
 VERSION=$1
-ADDON_VERSION=$2
 CURRENT_VERSION=`grep -o '^version = [0-9a-z.-]*' default/app.conf | awk '{print $3}'`
-echo "Bumping version from ${CURRENT_VERSION} to ${VERSION}"
 # Files where version needs to be bumped
 APPCONF=default/app.conf
 README=README.md
@@ -38,20 +35,18 @@ README=README.md
 git checkout develop
 
 # Release on GitHub
-git flow release start ${VERSION}
+git flow release start "${VERSION}"
 
 # Bump Versions
-release/bumpversion.sh ${VERSION} ${ADDON_VERSION}
+release/bumpversion.sh "${VERSION}"
 
 # Add and commit version bump
 git add ${README}
 git add ${APPCONF}
 git commit -m "Bump version number to ${VERSION}"
 
-git push origin release/${VERSION}
-
 # Finish up gitflow and merge back to develop and master
-git flow release finish '${VERSION}'
+git flow release finish "${VERSION}"
 
 echo "Git release is ready. You still need to push develop and master to origin with tags."
 echo "eg. git push origin master develop ${VERSION}"
